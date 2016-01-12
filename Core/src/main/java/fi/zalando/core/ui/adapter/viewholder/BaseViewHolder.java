54a425b097dfcd81091cad7faf7a3ab4bef2a6d2
Jon Duran <jon.duran@zalando.fi>
2016-01-12 16:32:07 +0200

    ZMAL-1031: Add bindData method to baseViewHolder

--- Core/src/main/java/fi/zalando/core/ui/adapter/viewholder/BaseViewHolder.java
+++ Core/src/main/java/fi/zalando/core/ui/adapter/viewholder/BaseViewHolder.java
@@ -1,5 +1,6 @@
 package fi.zalando.core.ui.adapter.viewholder;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -10,7 +11,7 @@ import butterknife.ButterKnife;
  *
  * Created by jduran on 12/01/16.
  */
-public abstract class BaseViewHolder extends RecyclerView.ViewHolder {
+public abstract class BaseViewHolder<T> extends RecyclerView.ViewHolder {
 
     /**
      * Constructor
@@ -23,4 +24,12 @@ public abstract class BaseViewHolder extends RecyclerView.ViewHolder {
         // Inflate items in ViewHolder with butterknife
         ButterKnife.bind(this, itemView);
     }
+
+    /**
+     * Binds the {@link T} model with the {@link RecyclerView.ViewHolder}
+     *
+     * @param modelToBind {@link T} model that will be bound with the {@link
+     *                    RecyclerView.ViewHolder}
+     */
+    public abstract void bindData(@NonNull T modelToBind);
 }
