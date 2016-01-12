e04589ecb6c28b28a8c3eabf1043f31191c4207c
Jon Duran <jon.duran@zalando.fi>
2016-01-12 15:53:24 +0200

    ZMAL-1031: Created a base adapter

---
+++ Core/src/main/java/fi/zalando/core/ui/adapter/BaseAdapter.java
@@ -0,0 +1,29 @@
+package fi.zalando.core.ui.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+/**
+ * Abstract activity that holds common methods usable by all the {@link RecyclerView.Adapter} on the
+ * app.
+ *
+ * Created by jduran on 12/01/16.
+ */
+public abstract class BaseAdapter<T, U extends RecyclerView.ViewHolder> extends RecyclerView
+        .Adapter<U> {
+
+    protected List<T> items;
+
+    /**
+     * Constructor
+     *
+     * @param items {@link List} of {@link T} to render in the list
+     */
+    protected BaseAdapter(@NonNull List<T> items) {
+
+        this.items = items;
+    }
+
+}
