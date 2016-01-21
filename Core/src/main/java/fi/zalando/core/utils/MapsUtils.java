06dd1c726fc792a3088a0d66d8dd62371b2332de
Hang Duy Khiem <hang.duy.khiem@zalando.fi>
2016-01-21 10:35:57 +0200

    ZMAL-1191: add encoding in MapsUtils, add unit test for MapsUtils

--- Core/src/main/java/fi/zalando/core/utils/MapsUtils.java
+++ Core/src/main/java/fi/zalando/core/utils/MapsUtils.java
@@ -56,4 +56,46 @@ public class MapsUtils {
 
         return path;
     }
+
+    /**
+     * Encodes a sequence of LatLngs into an encoded path string.
+     *
+     * @param path a list of polylines to encodePolyfromStringbuffer
+     */
+    public static String encodePoly(final List<LatLng> path) {
+        long lastLat = 0;
+        long lastLng = 0;
+
+        final StringBuffer result = new StringBuffer();
+
+        for (final LatLng point : path) {
+            long lat = Math.round(point.latitude * 1e5);
+            long lng = Math.round(point.longitude * 1e5);
+
+            long dLat = lat - lastLat;
+            long dLng = lng - lastLng;
+
+            encodePolyfromStringbuffer(dLat, result);
+            encodePolyfromStringbuffer(dLng, result);
+
+            lastLat = lat;
+            lastLng = lng;
+        }
+        return result.toString();
+    }
+
+    /**
+     * The some encoding is done here
+     *
+     * @param v parameters from lat and long
+     * @param result the stringBuffer to append more character
+     */
+    private static void encodePolyfromStringbuffer(long v, StringBuffer result) {
+        v = v < 0 ? ~(v << 1) : v << 1;
+        while (v >= 0x20) {
+            result.append(Character.toChars((int) ((0x20 | (v & 0x1f)) + 63)));
+            v >>= 5;
+        }
+        result.append(Character.toChars((int) (v + 63)));
+    }
 }
