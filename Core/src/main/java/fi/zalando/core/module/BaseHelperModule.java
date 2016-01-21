f32e41fe76e48697370cb2eff955093734c2e7ef
Jon Duran <jon.duran@zalando.fi>
2016-01-21 12:34:51 +0200

    ZMAL-1031: Add Core helper initialisers for Dagger

--- Core/src/main/java/fi/zalando/core/module/BaseHelperModule.java
+++ Core/src/main/java/fi/zalando/core/module/BaseHelperModule.java
@@ -1,8 +1,12 @@
 package fi.zalando.core.module;
 
+import android.content.Context;
+import android.location.LocationManager;
+
 import dagger.Module;
 import dagger.Provides;
 import fi.zalando.core.data.helper.GooglePlayServicesHelper;
+import fi.zalando.core.data.helper.LocationHelper;
 import fi.zalando.core.domain.helper.SubscriptionHelper;
 
 /**
@@ -22,6 +26,20 @@ public class BaseHelperModule {
     public GooglePlayServicesHelper provideGooglePlayServicesHelper() {
 
         return new GooglePlayServicesHelper();
+    }
+
+    /**
+     * Provides a {@link LocationHelper} instance dependency
+     *
+     * @param applicationContext {@link Context} of the app
+     * @param locationManager    {@link LocationManager} to inject
+     * @return {@link LocationHelper} instance
+     */
+    @Provides
+    public LocationHelper provideLocationHelper(Context applicationContext,
+                                                LocationManager locationManager) {
+
+        return new LocationHelper(applicationContext, locationManager);
     }
 
     /**
