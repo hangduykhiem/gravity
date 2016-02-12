d5f941ea47831346947bd6a7c33471891d9b060c
Jon Duran <jon.duran@zalando.fi>
2016-02-12 16:03:38 +0200

    ZMAL-1031: Add email validator utility class

--- Core/src/test/java/fi/zalando/core/utils/ValidationUtilsTest.java
+++ Core/src/test/java/fi/zalando/core/utils/ValidationUtilsTest.java
@@ -1,28 +1,16 @@
 package fi.zalando.core.utils;
 
-import android.content.Context;
-import android.graphics.Point;
-import android.location.LocationManager;
 import android.os.Build;
-import android.telephony.TelephonyManager;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import fi.zalando.core.BuildConfig;
 
-import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNull;
 import static junit.framework.Assert.assertTrue;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
 
 /**
  * Test class for {@link ValidationUtils}
