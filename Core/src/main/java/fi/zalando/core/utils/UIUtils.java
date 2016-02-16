1c0916f57aaf6dc59e20a1de1c4748e3ec6fbdc2
Ville Räisänen <ville.raisanen@zalando.fi>
2016-02-16 11:26:42 +0200

    ZMAL-1353: Comment-fix

--- Core/src/main/java/fi/zalando/core/utils/UIUtils.java
+++ Core/src/main/java/fi/zalando/core/utils/UIUtils.java
@@ -20,7 +20,7 @@ public class UIUtils {
     }
 
     /**
-     * Runs the given {@link Runnable} synchronously in the UI thread once the layout is complete.
+     * Runs the given {@link Runnable} in the UI thread once the layout is complete.
      * It registers a {@link ViewTreeObserver.OnGlobalLayoutListener} to the given {@link View},
      * runs the Runnable, and finally unregisters the said listener. This method needs to be called
      * before the layout is finished, e.g. in onCreate.
