e7935033fa64cf83c3f8e6f5e3fc7b41a6ce3d50
Ville Räisänen <ville.raisanen@zalando.fi>
2016-02-18 15:37:47 +0200

    ZMAL-1353: Added Observable for listening when the UI is ready.

--- Core/src/main/java/fi/zalando/core/ui/view/BaseView.java
+++ Core/src/main/java/fi/zalando/core/ui/view/BaseView.java
@@ -1,5 +1,7 @@
 package fi.zalando.core.ui.view;
 
+import rx.Observable;
+
 /**
  * Base interface that will be extended by Activities and Fragments that will define the actions
  * they are able to provide to presenters to render and react with the data.
@@ -12,6 +14,13 @@ package fi.zalando.core.ui.view;
  * Created by jduran on 24/11/15.
  */
 public abstract interface BaseView {
+
+    /**
+     * This method provides an Observable for Presenters to listen to, which will notify them when
+     * the UI is initialised.
+     * @return {@link Observable}
+     */
+    public Observable<Void> getOnViewReady();
 
     /**
      * This is used to initialize the View for the first time. This method is called at the end of
