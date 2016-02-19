cc2ca06cca32601963748fbe8bc0c44f9098be24
Jon Duran <jon.duran@zalando.fi>
2016-02-19 10:44:20 +0200

    ZMAL-1031: Add realm object that stores automatically the saved time

--- Core/src/main/java/fi/zalando/core/data/model/BaseRealmObject.java
+++ Core/src/main/java/fi/zalando/core/data/model/BaseRealmObject.java
@@ -6,11 +6,21 @@ import io.realm.RealmObject;
 
 /**
  * Abstract {@link RealmObject} that keeps saved when the object was saved in the persistence
- * storage
+ * storage. NOTE: Abstract keyword is not used because realm processor fails.
  */
-public abstract class BaseRealmObject extends RealmObject {
+public class BaseRealmObject extends RealmObject {
 
     private long savedDate;
+
+    /**
+     * Returns the saved date
+     *
+     * @return {@link Long} with the saved date
+     */
+    public long getSavedDate() {
+
+        return savedDate;
+    }
 
     /**
      * Sets the saved time
@@ -29,8 +39,10 @@ public abstract class BaseRealmObject extends RealmObject {
      * @param timeUnit {@link TimeUnit} of the given live time
      * @return {@link Boolean} indicating if the object is expired
      */
-    public boolean isExpired(long liveTime, TimeUnit timeUnit) {
-
-        return savedDate + timeUnit.toMillis(liveTime) <= System.currentTimeMillis();
+    public static boolean hasExpired(BaseRealmObject realmObject, long liveTime, TimeUnit
+            timeUnit) {
+        // Needs to be static because realm processor fails since it is not accessing the method
+        return realmObject.getSavedDate() + timeUnit.toMillis(liveTime) <= System
+                .currentTimeMillis();
     }
 }
