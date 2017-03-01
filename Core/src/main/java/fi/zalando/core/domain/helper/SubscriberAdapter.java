dcd7ce2249385022cca8cc24c2c3d3f1055cb09a
Jon Duran <jon.duran@zalando.fi>
2017-03-01 17:10:26 +0200

    #306 Subscriber to Observer

--- Core/src/main/java/fi/zalando/core/domain/helper/SubscriberAdapter.java
+++ Core/src/main/java/fi/zalando/core/domain/helper/SubscriberAdapter.java
@@ -1,15 +1,26 @@
 package fi.zalando.core.domain.helper;
 
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 /**
- * Base {@link Subscriber} class whenever all methods implemented are required but only react to
- * one of them. To react to it, override needed function
+ * Base {@link Observer} class whenever all methods implemented are required but only react to one
+ * of them. To react to it, override needed function
  *
  * Created by jduran on 11/01/16.
  */
-public class SubscriberAdapter<T> implements Subscriber<T> {
+public class SubscriberAdapter<T> implements Observer<T> {
+
+  @Override
+  public void onSubscribe(Disposable d) {
+    // no-op by default.
+  }
+
+  @Override
+  public void onNext(T t) {
+    // no-op by default.
+  }
 
   @Override
   public void onError(Throwable e) {
@@ -18,16 +29,6 @@ public class SubscriberAdapter<T> implements Subscriber<T> {
 
   @Override
   public void onComplete() {
-    // no-op by default.
-  }
-
-  @Override
-  public void onSubscribe(Subscription s) {
-    // no-op by default.
-  }
-
-  @Override
-  public void onNext(T t) {
     // no-op by default.
   }
 }
