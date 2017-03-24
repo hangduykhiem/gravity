c7563b438541456c63b2be4a751851c5e6962d7c
izorin <ilya.zorin@zalando.fi>
2017-03-24 13:06:36 +0200

    #319 Add comment to TextUtils.isEmpty

--- Core/src/main/java/fi/zalando/core/utils/TextUtils.java
+++ Core/src/main/java/fi/zalando/core/utils/TextUtils.java
@@ -4,6 +4,11 @@ import android.support.annotation.Nullable;
 
 public class TextUtils {
 
+  /**
+   * Returns true if the string is null or 0-length.
+   * @param str the string to be examined
+   * @return true if str is null or zero length
+   */
   public static boolean isEmpty(@Nullable CharSequence str) {
     return str == null || str.length() == 0;
   }
