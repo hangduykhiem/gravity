eb0d52af328afe8f60c81a96a571b5b7bc3c4a32
izorin <ilya.zorin@zalando.fi>
2017-03-24 12:52:03 +0200

    #319 Fix checkstyle

--- Core/src/main/java/fi/zalando/core/utils/TextUtils.java
+++ Core/src/main/java/fi/zalando/core/utils/TextUtils.java
@@ -5,9 +5,6 @@ import android.support.annotation.Nullable;
 public class TextUtils {
 
   public static boolean isEmpty(@Nullable CharSequence str) {
-    if (str == null || str.length() == 0)
-      return true;
-    else
-      return false;
+    return str == null || str.length() == 0;
   }
 }
